058f8bee763b2c4c1a407688cf76dc33f32a558b
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-28 08:49:53 -0400

    Add Workouts controller, and index and create actions in that controller

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,19 @@
+class Api::WorkoutsController < ApplicationController
+
+  def index
+    @workouts = Workout.all
+    render json: @workouts
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      render json: @workout
+    else
+      render json: {errors: @workout.errors}
+      # render json: {message: "There's a problem getting your flex on. Please try again!"}
+    end
+  end
+  
+
+end
