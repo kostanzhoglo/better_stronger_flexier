7980b8bde3d8fe09f6c67f9483f6a694e0d9dbec
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-28 12:03:02 -0400

    Add some filler text to index.html to test that everything is working ok

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          oh really fool
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        yeah that's right fooooool
       </div>
     );
   }
