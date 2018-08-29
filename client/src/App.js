a989bf4f7e798b2412bb4517e432b870fb71c556
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-29 16:42:03 -0400

    Add react-router-dom, Router and Route(s) to App.js, and create following components- Hello, Navbar, TipsPage, WorkoutsPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Better, Stronger, Flexier!</h1>
-        </header>
-        <p className="App-intro">
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <header className="App-header">
+            <h1 className="App-title">Welcome to Better, Stronger, Flexier!</h1>
+            <Route exact path="/" component={Hello} />
+            <Route exact path="/tips" component={TipsPage} />
+            <Route exact path="/workouts" component={WorkoutsPage} />
+
+          </header>
+          <p className="App-intro">
+
+          </p>
+        </div>
+    </Router>
     );
   }
 }
