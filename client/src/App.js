e48cfed5e201aa5a30f94415017dd61447b910b8
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-29 15:31:23 -0400

    Edited how I was importing rootReducer and bootstrap because of errors, began work on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          oh really fool
+          <h1 className="App-title">Welcome to Better, Stronger, Flexier!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        yeah that's right fooooool
       </div>
     );
   }
