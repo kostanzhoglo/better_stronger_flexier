e16e1edf498bcd115901a5b2b6a383103796585d
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-29 14:46:12 -0400

    Add Import language into index.js for previous new programs being imported, create store and add Provider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
