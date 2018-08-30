b04f35fc429dbc6a1c483327eb5c5329228c4d85
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-30 10:48:51 -0400

    Add link styling and NavLinks to Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
+
+    const link = {
+      width: '100px',
+      padding: '12px',
+      margin: '0 6px 6px',
+      background: 'pink',
+      textDecoration: 'none',
+      color: 'white',
+    }
+
     return (
-      "I'm the Navbar!"
+      <div>
+        <NavLink
+          to="/"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue',
+            color: 'red'
+        }}>
+          Home
+        </NavLink>
+        <NavLink
+          to="/tips"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue',
+            color: 'red'
+        }}>
+          Workout Tips
+        </NavLink>
+        <NavLink
+          to="/workouts"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue',
+            color: 'red'
+        }}>
+          Workouts
+        </NavLink>
+      </div>
     )
   }
 
