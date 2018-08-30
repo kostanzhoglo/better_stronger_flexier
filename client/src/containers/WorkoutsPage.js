a7829341148160268bb20f640237c5c38b7e353f
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-30 15:12:34 -0400

    start building out WorkoutsPage component. Added state, and some rendering

--- client/src/containers/WorkoutsPage.js
+++ client/src/containers/WorkoutsPage.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 
 class WorkoutsPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      workouts: [
+        {
+          name: 'chest',
+          content: 'pushups 4sets x 12reps, \n neck stretch 1x'
+        }
+      ]
+    }
+  };
+
   render() {
-      const work = "Work It Out!!!"
+      const workouts = this.state.workouts.map(workout => (
+        <Workout name={workout.name} content={workout.content} />
+      ))
     return (
 
       <div>
         <p>
           WORK IT OUT
         </p>
-        {work}
+        {workouts}
       </div>
     )
   }
