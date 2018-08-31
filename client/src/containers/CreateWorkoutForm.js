e6f3b9ba1c724958e0142eb9ac6a620d5ee83e8b
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-31 11:14:35 -0400

    Start stub out of CreateWorkoutForm and workouts.js actions & install isomorphic-fetch

---
+++ client/src/containers/CreateWorkoutForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addWorkout } from '../actions/workouts';
+
+class CreateWorkoutForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      content: '',
+      completions: ''
+    }
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
