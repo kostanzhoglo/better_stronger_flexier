0229d28ccc97fa7d577f85df78e062e21953075a
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-31 11:51:17 -0400

    Add form to CreateWorkoutForm, and action addWorkout to workouts.js

--- client/src/actions/workouts.js
+++ client/src/actions/workouts.js
@@ -1,1 +1,19 @@
 import fetch from 'isomorphic-fetch';
+
+export function addWorkout (workout) {
+  const request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ workout: workout })
+  }
+
+
+  return (dispatch) => {
+    dispatch({ type: 'ADDING_WORKOUT' });
+    return fetch(`http://localhost:3000/api/workouts`, request)
+      .then(response => response.json())
+      .then(workoutsResponse => dispatch({ type: 'CREATE_WORKOUT', workouts: workoutsResponse }));
+  }
+}
--- client/src/containers/CreateWorkoutForm.js
+++ client/src/containers/CreateWorkoutForm.js
@@ -15,7 +15,23 @@ class CreateWorkoutForm extends Component {
 
   render() {
     return (
-      
+      <div>
+        <h2>Create a New Workout</h2>
+          <form>
+            <p>
+              <label>Workout Name:</label>
+              <input type="text" id="name" name="name" />
+            </p>
+            <p>
+              <label>Exercises / Stretches:</label>
+              <textarea name="content" rows="20" cols="40"></textarea>
+            </p>
+            <p>
+              <button type="submit">Add Workout</button>
+            </p>
+          </form>
+
+      </div>
     )
   }
 }
