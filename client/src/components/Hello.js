09bfaba2624300d98b97facbabe297ed1ba69bdc
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-31 15:13:09 -0400

    Make Hello an SFC

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Hello extends Component {
-  render() {
+const Hello = () => {
     return (
       <div>
         Welcome to Better, Stronger, Flexier
@@ -16,7 +15,7 @@ class Hello extends Component {
         </p>
       </div>
     )
-  }
+
 
 }
 
