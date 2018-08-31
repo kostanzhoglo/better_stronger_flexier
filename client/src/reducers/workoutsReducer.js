beec5d3445f413931724f41a5fe4bd7cdf0cba0e
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-31 12:14:52 -0400

    Add two actions to workoutsReducer

--- client/src/reducers/workoutsReducer.js
+++ client/src/reducers/workoutsReducer.js
@@ -1,1 +1,11 @@
+export default function workoutsReducer (state = {loading: false, workouts: []}, action) {
 
+  switch (action.type) {
+    case 'ADDING_WORKOUT':
+      return {...state, loading: true}
+    case 'CREATE_WORKOUT':
+      return {...state, workouts: action.workouts}
+    default:
+      return state;
+  }
+}
