3eb49c91b8291df55c9f4cb508f9ac6148e0dd2f
kostanzhoglo <jeffreywithers@gmail.com>
2018-09-02 06:20:36 -0400

    Delete commented out old hard-coded lines from WorkoutsPage

--- client/src/containers/WorkoutsPage.js
+++ client/src/containers/WorkoutsPage.js
@@ -9,38 +9,14 @@ import Patience from '../components/Patience';
 
 
 class WorkoutsPage extends Component {
-  // constructor(props) {
-  //   super(props);
 
-  //   this.state = {
-  //     workouts: [
-  //       {
-  //         name: 'chest',
-  //         content: 'pushups 4sets x 12reps, neck stretch 1x'
-  //       },
-  //       {
-  //         name: 'cardio',
-  //         content: 'jump rope 5 minutes, hamstring stretch'
-  //       },
-  //       {
-  //         name: 'back',
-  //         content: 'pullups 5sets, 6-6-4-3-2, 1-arm row 4sets x 8 reps, wrist strength 1set x 15'
-  //       }
-  //     ]
-  //   }
-  // };
 
   componentDidMount() {
     this.props.getWorkouts()
   }
 
   render() {
-      // const workouts = this.state.workouts.map(workout => (
-      //   <Workout title={workout.name} content={workout.content} />
-      // ))
-      // const workouts = this.props.workouts.map(workout => (
-      //   <WorkoutLinks workout={workout} />
-      // ))
+
     return (
       <div>
           <p>
@@ -60,6 +36,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { getWorkouts })(WorkoutsPage);
-
-{/* <WorkoutLinks workouts={this.props.workouts} /> */}
-{/* {workouts} */}
