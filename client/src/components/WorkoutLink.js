49142b1d3b67394fd2716ee9a83732f2ba00e5c1
kostanzhoglo <jeffreywithers@gmail.com>
2018-09-13 13:21:04 -0400

    Add state and physical Like button and handleOnClick() to WorkoutLink

--- client/src/components/WorkoutLink.js
+++ client/src/components/WorkoutLink.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 
 class WorkoutLink extends Component {
 
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      workout: props.workout,
+      likes: 0
+    }
+  }
+
+  handleOnClick = (e) => {
+    e.preventDefault();
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
 
   render() {
-
     return (
-      <div>WorkoutLink Component</div>
+      <div>
+        <button onClick={this.handleOnClick}>Like</button>
+        NEW Likes: {this.state.likes}
+      </div>
     )
   }
 }
