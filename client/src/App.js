f6b001343f2894b6ee727af896e62c84e3a83bfb
kostanzhoglo <jeffreywithers@gmail.com>
2018-09-13 06:36:32 -0400

    Add background image photos, and some css styling for app background

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+// import marathon from './assets/background_imgs/86_marathon.jpg'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Hello from './components/Hello';
 import Navbar from './components/Navbar';
@@ -20,10 +21,12 @@ const App = () => {
             Better, Stronger, Flexier!
           </h1>
         </header>
+        <div className="background">
           <Route exact path="/" component={Hello} />
           <Route exact path="/tips" component={TipsPage} />
           <Route path="/workouts" component={WorkoutsPage} />
           {/* <Route path="/workouts/new" component={CreateWorkoutForm} /> */}
+        </div>
       </div>
     </Router>
   );
